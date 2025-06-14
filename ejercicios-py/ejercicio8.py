bab0805dedc1bf27a77aeb9c349bc022521dd72a
Danild Zambrana <42362162+DanildZambrana@users.noreply.github.com>
2025-06-13 20:32:52 -0600

    Se agrega solución para el ejercicio 8 de Python
    **Generador de números aleatorios**
    - Genera una lista de entre 1 y 20 números aleatorios entre un rango definido (1 a 100).
    - Imprime la lista generada, su cantidad de elementos y la suma total.

---
+++ ejercicios-py/ejercicio8.py
@@ -0,0 +1,23 @@
+import random
+
+"""
+Genera una lista de entre 1 y 20 números aleatorios
+:param min:
+:param max:
+"""
+def generate_random_numbers(min, max):
+    numbers = []
+    amount = random.randint(1, 20)
+    
+    for i in range(amount):
+        numbers.append(random.randint(min, max))
+    
+    return numbers
+
+data = generate_random_numbers(1, 100)
+
+print(f"Se generaron {len(data)} números aleatorios entre 1 y 100:")
+
+print(f"Números generados: {', '.join(map(str, data))}")
+
+print(f"La suma de los números es: {sum(data)}")
